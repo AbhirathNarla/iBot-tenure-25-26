596fa24ff56770f78128813b24a4cfb464007405
AbhirathNarla <abhirathnarla2007@gmail.com>
2026-02-04 19:16:04 +0530

    Create clapswitch.cpp

---
+++ workshop/clapswitch.cpp
@@ -0,0 +1,14 @@
+//2 is DO of sound sensor
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);  
+}
+
+void loop() {
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(13, HIGH); 
+    delay(2000);
+    digitalWrite(13,LOW);
+    delay(2000);
+}
