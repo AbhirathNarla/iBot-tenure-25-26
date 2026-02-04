d0a892976f0daab7c66c46f2e99b7ba2fe55e078
AbhirathNarla <abhirathnarla2007@gmail.com>
2026-02-04 16:31:45 +0530

    Create ldrdigital.cpp

---
+++ workshop/ldrdigital.cpp
@@ -0,0 +1,15 @@
+// C++ code
+//
+pinldr = 6
+void setup()
+{
+  pinMode(6, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println(digitalRead(6));
+  delay(1000);
+                 
+}
